be9308e00b2859e91f5371eb1e9bbc08e113b82e
Hemant8120 <hemant8120g@gmail.com>
2023-04-25 02:43:04 +0530

    Model Trainer

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -53,13 +53,3 @@ class DataIngestion:
         except Exception as e:
             logging.info('Exception Occured at Data Ingestion Stage')
             raise CustomException(e, sys)
-
-
-# Run Data Integration
-
-if __name__ == '__main__':
-    obj = DataIngestion()
-    train_data_path, test_data_path = obj.initiate_data_ingestion()
-    data_transformation = DataTransformation()
-    train_arr, test_arr, _ = data_transformation.initiate_data_transformation(
-        train_data_path, test_data_path)
